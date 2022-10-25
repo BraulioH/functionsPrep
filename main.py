660318c3c31ebda4ebcb94503da5f87b37167ae8
Braulio Hernandez <113930423+BraulioH@users.noreply.github.com>
2022-10-25 17:27:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from add import add_fruit
+from multiply import multiply_fruit
+from divide import divide_fruit
+
+
+apples = int(input("How many apples?"))
+oranges = in(input("How many oranges?"))
+
+#function called for adding apples and orages
+add_fruit(apples, oranges)
+
+
+#function for dividing apples and oranges
+divide_fruit(apples, oranges)
+
+#function for multiplying apples and oranges
+multiply_fruit(apples, oranges)
